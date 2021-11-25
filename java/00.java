e6b6efac746293ced36c153aa019ba44ee4f23db
Azizullo Temirov <63806547+azakapro@users.noreply.github.com>
2021-11-25 07:36:18 +0500

    Create 00.java

---
+++ java/00.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.print("Hello, World."+"\n"+str);
+    }
+}
